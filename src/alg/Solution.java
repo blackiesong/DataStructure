86a41f8d39251afb2569db66d7a00a7ae83dc15d
blackiesong <277464327@qq.com>
2017-07-17 17:38:07 +0800

    java 实现replace方法 将空格转换成%20

---
+++ src/alg/Solution.java
@@ -0,0 +1,33 @@
+package alg;
+
+public class Solution {
+	public static void main(String[] args) {
+		Solution s=new Solution();
+		StringBuffer b=new StringBuffer();
+		b.append("we are happy ");
+		System.out.println(s.replaceSpace(b));
+	}
+	public String replaceSpace(StringBuffer str){
+		int spacenum=0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i)==' ') {
+				spacenum++;
+			}
+		}
+		int oldindex=str.length()-1;
+		int newlenth=str.length()+spacenum*2;
+		int newindex=newlenth-1;
+		str.setLength(newlenth);
+		for (; oldindex>0&&oldindex<newindex; --oldindex) {
+			if (str.charAt(oldindex)==' ') {
+				str.setCharAt(newindex--, '0');
+				str.setCharAt(newindex--, '2');
+				str.setCharAt(newindex--, '%');
+			}else {
+				str.setCharAt(newindex--, str.charAt(oldindex));
+			}
+		}
+		
+		return str.toString();
+	}
+}
